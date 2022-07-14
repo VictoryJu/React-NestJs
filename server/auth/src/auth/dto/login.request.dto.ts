1affe6170a3a2add33351daec91c78b66b7b0ea8
Ju <68391427+VictoryJu@users.noreply.github.com>
2022-07-14 23:14:15 +0900

    Delete server/auth directory
    delete

--- server/auth/src/auth/dto/login.request.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { User } from 'src/uesrs/user.schema';
-
-export class LoginRequestDto extends PickType(User, [
-  'id',
-  'password',
-] as const) {}
--- server/auth/src/uesrs/dto/users.request.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
-import { User } from '../user.schema';
-
-export class UserRequestDto extends PickType(User, [
-  'id',
-  'password',
-] as const) {}
