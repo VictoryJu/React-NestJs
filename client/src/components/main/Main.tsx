686ec25880050a52fbb324cab48b6f58e499cfed
VictoryJu <kob0805@naver.com>
2022-08-04 23:57:25 +0900

    React uerQuery 적용중

--- client/src/components/main/Main.tsx
+++ client/src/components/main/Main.tsx
@@ -1,7 +1,17 @@
-import React, { Component } from "react";
-
-export default class Main extends Component {
-  render() {
-    return <div>Main</div>;
-  }
+import React, { useEffect, useState } from "react";
+import { useQueries } from "react-query";
+import Api from "../../service/Api";
+//수정해야함
+export default function Main() {
+  const getUserData = async () => {
+    try {
+      const res = await Api.get("/users");
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const { status, data, error } = useQueries("users", getUserData());
+  useEffect(() => {}, []);
+  return <div>Main</div>;
 }
